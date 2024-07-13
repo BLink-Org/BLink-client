4ed1c70bf04e5234ac94840c4bac575ce558eaa8
TaegeonKim <aksen5240@gmail.com>
2024-07-13 23:09:57 +0900

    feat: Set up initial page for WebView archive

--- src/screens/Mypage.tsx
+++ src/screens/Mypage.tsx
@@ -1,46 +1,23 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {useThemeStore} from '@/store/useThemeStore';
-import ThemeBackground from '@/components/common/ThemeBackground';
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {View, Button} from 'react-native';
+import {type RootStackNavigationProp} from '@/types/navigation';
 
-const Mypage = () => {
-  const {theme, setTheme, asyncSetTheme} = useThemeStore();
-
-  const handleSetTheme = (themeNumber: number) => {
-    setTheme(themeNumber);
-    void asyncSetTheme(themeNumber);
-  };
+const MyPage = () => {
+  const navigation = useNavigation<RootStackNavigationProp>();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ThemeBackground />
-      <View style={styles.content}>
-        <Button title="Theme 1" onPress={() => handleSetTheme(1)} />
-        <Button title="Theme 2" onPress={() => handleSetTheme(2)} />
-        <Button title="Theme 3" onPress={() => handleSetTheme(3)} />
-        <Button title="Theme 4" onPress={() => handleSetTheme(4)} />
-        <Text style={[styles.text, {color: theme.TEXT_COLOR}]}>
-          현재 테마 메인 색: {theme.MAIN_COLOR}
-        </Text>
-      </View>
-    </SafeAreaView>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button
+        title="테마 설정"
+        onPress={() => navigation.navigate('ThemeSetting')}
+      />
+      <Button
+        title="WebViewTest"
+        onPress={() => navigation.navigate('WebViewTest')}
+      />
+    </View>
   );
 };
 
-export default Mypage;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  text: {
-    fontSize: 18,
-    marginTop: 20,
-  },
-
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+export default MyPage;
---
+++ src/screens/WebViewTest.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const WebViewTest = () => {
+  return (
+    <View style={styles.container}>
+      <Text>WebViewTest</Text>
+    </View>
+  );
+};
+
+export default WebViewTest;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
