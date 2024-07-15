d0e7601275e618ce40a323a1138c4f5e1fca98f9
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2024-07-15 21:57:56 +0900

    feat: Implement font system setup and default theme setup (#10)
    * rename: Reorganize icon folder location
    
    * feat: Implement default theme setup
    
    * feat: Implement text design system setup

---
+++ src/constants/fonts.ts
@@ -0,0 +1,56 @@
+export default {
+  TITLE: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 27,
+    lineHeight: 40.5,
+    letterSpacing: 0,
+  },
+  SUBTITLE: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 20,
+    lineHeight: 30,
+    letterSpacing: -0.2,
+  },
+  BODY1_SEMIBOLD: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 17,
+    lineHeight: 25.5,
+    letterSpacing: -0.2,
+  },
+  BODY1_MEDIUM: {
+    fontFamily: 'Pretendard-Medium',
+    fontSize: 17,
+    lineHeight: 25.5,
+    letterSpacing: -0.4,
+  },
+  BODY2_SEMIBOLD: {
+    fontFamily: 'Pretendard-SemiBold',
+    fontSize: 14,
+    lineHeight: 21,
+    letterSpacing: -0.2,
+  },
+  BODY2_MEDIUM: {
+    fontFamily: 'Pretendard-Medium',
+    fontSize: 14,
+    lineHeight: 21,
+    letterSpacing: -0.2,
+  },
+  BODY2_REGULAR: {
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 14,
+    lineHeight: 21,
+    letterSpacing: -0.2,
+  },
+  CAPTION_MEDIUM: {
+    fontFamily: 'Pretendard-Medium',
+    fontSize: 12,
+    lineHeight: 18,
+    letterSpacing: 0,
+  },
+  CAPTION_REGULAR: {
+    fontFamily: 'Pretendard-Regular',
+    fontSize: 12,
+    lineHeight: 18,
+    letterSpacing: 0,
+  },
+};
