ebba5271f02f62f227102bedd00bb978e595842b
TaegeonKim <aksen5240@gmail.com>
2024-07-15 20:58:18 +0900

    design: Design global navigation bar

---
+++ src/assets/bottom-tab/index.ts
@@ -0,0 +1,7 @@
+import HomeIcon from './home.svg';
+import BookmarkIcon from './bookmark.svg';
+import LinkIcon from './link.svg';
+import MypageIcon from './mypage.svg';
+import SearchIcon from './search.svg';
+
+export {HomeIcon, BookmarkIcon, LinkIcon, MypageIcon, SearchIcon};
