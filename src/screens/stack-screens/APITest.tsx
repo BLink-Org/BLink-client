7f5ee8c63ff36be9afcd15feb179e838cfbc0002
TaegeonKim <aksen5240@gmail.com>
2024-08-10 16:44:11 +0900

    fix: Fix token refresh and retry on 401 error using axios interceptor

--- src/screens/stack-screens/APITest.tsx
+++ src/screens/stack-screens/APITest.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {useHealthCheck} from '@/api/hooks/useTest';
 import {useUserInfo} from '@/api/hooks/useUser';
+import {useUserStore} from '@/store/useUserStore';
 
 const APITest = () => {
   const {data: healthCheckData} = useHealthCheck();
-  const {data: userInfoData} = useUserInfo();
+  const {data: userInfoData, error} = useUserInfo();
+
+  if (error) {
+    console.error('Error while fetching user info:', error);
+  }
+
+  // accessToken refreshToken 가져오기
+  const {accessToken, refreshToken} = useUserStore.getState();
 
   return (
     <SafeAreaView>
       <Text>API Test</Text>
       <Text>{JSON.stringify(healthCheckData)}</Text>
       <Text>{JSON.stringify(userInfoData)}</Text>
+      <Text>accessToken: {accessToken}</Text>
+      <Text>refreshToken: {refreshToken}</Text>
     </SafeAreaView>
   );
 };
