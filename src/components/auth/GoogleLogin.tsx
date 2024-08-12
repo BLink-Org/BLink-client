6622bbdcdc2ad7d503292d9e8bc9689f4a19a160
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2024-08-12 21:16:21 +0900

    feat: Implement onboarding UI  (#42)
    * feat: Implement the onboarding pages excluding carousel
    
    * feat: Implement carousel component for onboarding page
    
    ---------
    
    Co-authored-by: Sujong Kwak <kwaksj329@gmail.com>

--- src/components/auth/GoogleLogin.tsx
+++ src/components/auth/GoogleLogin.tsx
@@ -1,5 +1,7 @@
+import {useEffect} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {useEffect, useState} from 'react';
-import {Button, SafeAreaView} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {
   GoogleSignin,
   isErrorWithCode,
@@ -9,6 +11,8 @@ import {CLIENT_ID, IOS_CLIENT_ID} from '@env';
 import {useGoogleLogin} from '@/api/hooks/useAuth';
 import {useUserStore} from '@/store/useUserStore';
 import {type TokensSchema} from '@/types';
+import {GoogleLogoIcon} from '@/assets/icons/onboarding';
+import {FONTS} from '@/constants';
 import {initializeAmplitude, trackEvent} from '@/utils/amplitude-utils';
 
 const GoogleLogin = () => {
@@ -63,10 +67,29 @@ const GoogleLogin = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Button title="Sign In with Google" onPress={handleLoginPress} />
-    </SafeAreaView>
+    <TouchableOpacity onPress={handleLoginPress}>
+      <View style={styles.logo}>
+        <GoogleLogoIcon />
+        <Text style={styles.logoText}>Google로 시작하기</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 export default GoogleLogin;
+
+const styles = StyleSheet.create({
+  logo: {
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 12,
+    height: 54,
+    borderRadius: 100,
+  },
+  logoText: {
+    color: '#000',
+    ...FONTS.BODY1_SEMIBOLD,
+  },
+});
