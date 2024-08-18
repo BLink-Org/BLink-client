fbdaf6e1ecb2882f48b9a493b0e5ecaebe7e2140
TaegeonKim <aksen5240@gmail.com>
2024-08-18 13:23:43 +0900

    fix: Modify share functionality to support sharing on Android

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,4 +1,14 @@
-import {Share} from 'react-native';
+import {Platform, Share} from 'react-native';
+
+// TODO: 문구 수정 필요
+// export const shareUrl = (currentUrl: string) => {
+//   Share.share({
+//     if (Platform.OS === 'android') {
+//     message: `Check out this website: ${currentUrl}`,
+//     }
+//     else {
+//     url: currentUrl
+//     }
 
 // hostname 추출 util function
 export function extractHostname(url: string): string {
@@ -19,32 +29,37 @@ export function isValidUrl(url: string): boolean {
   return regex.test(url);
 }
 
-// TODO: 문구 수정 필요
 export const shareUrl = (currentUrl: string) => {
-  Share.share({
-    // message: `Check out this website: ${currentUrl}`,
-    url: currentUrl,
-  }).catch(error => {
-    console.error('Error sharing: ', error.message);
+  const shareOptions = Platform.select({
+    android: {message: `${currentUrl}`},
+    ios: {url: currentUrl},
   });
+
+  if (shareOptions) {
+    Share.share(shareOptions).catch(error => {
+      console.error('Error sharing:', error.message);
+    });
+  } else {
+    console.error('Error: shareOptions is undefined');
+  }
 };
 
-export const shareUrlHome = async (currentUrl: string) => {
-  return await Share.share({
-    // message: `Check out this website: ${currentUrl}`,
-    url: currentUrl,
-  })
-    .then(result => {
-      if (
-        result.action === Share.sharedAction ||
-        result.action === Share.dismissedAction
-      ) {
-        console.log('Share action finished:', result.action);
-        return result;
-      }
-    })
-    .catch(error => {
-      console.error('Error sharing:', error.message);
-      throw error; // 에러를 다시 던져서 onSelect에서 처리할 수 있도록 함
-    });
-};
+// export const shareUrlHome = async (currentUrl: string) => {
+//   return await Share.share({
+//     // message: `Check out this website: ${currentUrl}`,
+//     url: currentUrl,
+//   })
+//     .then(result => {
+//       if (
+//         result.action === Share.sharedAction ||
+//         result.action === Share.dismissedAction
+//       ) {
+//         console.log('Share action finished:', result.action);
+//         return result;
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Error sharing:', error.message);
+//       throw error; // 에러를 다시 던져서 onSelect에서 처리할 수 있도록 함
+//     });
+// };
