996c1cb9d0f9a1cd4952e80c33e11327b1465a4b
TaegeonKim <aksen5240@gmail.com>
2025-03-27 23:54:59 +0900

    chore: Update IOS version

--- ios/BLinkApp/AppDelegate.mm
+++ ios/BLinkApp/AppDelegate.mm
@@ -29,10 +29,13 @@
    
     // register LottieSplashScreen to RNSplashScreen
     [RNSplashScreen showLottieSplash:animationUIView inRootView:rootView];
-    // casting UIView type to AnimationView type
-    AnimationView *animationView = (AnimationView *) animationUIView;
+
+    // casting UIView type to LottieAnimationView type
+    LottieAnimationView *animationView = (LottieAnimationView *)animationUIView;
+
     // play
     [t playWithAnimationView:animationView];
+
     // If you want the animation layout to be forced to remove when hide is called, use this code
     [RNSplashScreen setAnimationFinished:true];
   }
